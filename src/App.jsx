415d714b434c949f4c56364ccc7c505f7fa0fe14
墨綠BG <s5460703@gmail.com>
2023-01-11 20:05:04 +0800

    Added "Please rotate to portrait" prompt
    Added "Please rotate to portrait" prompt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, {useState, useEffect} from 'react';
 import baseLayer from './img/home page/base layer.png';
 import bulletinBoard from './img/home page/bulletin board.png';
 import calendar from './img/home page/calendar.png';
@@ -10,16 +9,22 @@ import './App.css';
 
 
 export default function App(){
-    
-    return(
-        <div>
-            <img src={baseLayer} className="baseLayer"/>
-            <img src={bulletinBoard} className="bulletinBoard item"/>
-            <img src={calendar} className="calendar item"/>
-            <img src={garbageCan} className="garbageCan item"/>
-            <img src={paperAirplane} className="paperAirplane item"/>
-            <img src={pottedPlant} className="pottedPlant item"/>
-            <img src={todoList} className="todoList item"/>
-        </div>
-    );
+    if(window.innerWidth > window.innerHeight){
+        return(
+            <div className="hint"><h2>請將螢幕轉為縱向或使用手機檢視並重整頁面</h2></div>
+        );
+    }
+    else{
+        return(
+            <div className="view">
+                <img src={baseLayer} className="baseLayer"/>
+                <img src={bulletinBoard} className="bulletinBoard item"/>
+                <img src={calendar} className="calendar item"/>
+                <img src={garbageCan} className="garbageCan item"/>
+                <img src={paperAirplane} className="paperAirplane item"/>
+                <img src={pottedPlant} className="pottedPlant item"/>
+                <img src={todoList} className="todoList item"/>
+            </div>
+        );
+    }    
 }
