212942b86ca454384bfc763d64df5b1c1800a731
墨綠BG <s5460703@gmail.com>
2023-03-12 20:17:27 +0800

    renew setupProxy.js 2nd
    renew setupProxy.js 2nd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(app){
   app.use(
     createProxyMiddleware('/api/v1/', {
-      target: '116.204.121.9:1729/',
+      target: 'http://116.204.121.9:1729/',
       changeOrigin: true
     })
   );
