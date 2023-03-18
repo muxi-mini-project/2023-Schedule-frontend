650032bb47e788f154bfc0c6aa4ab88a821ea361
墨綠BG <s5460703@gmail.com>
2023-03-18 17:03:45 +0800

    renew index.js
    renew index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from "react";
-import  ReactDOM  from "react-dom";
-import App from './App'
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App';
+import '../src/assets/style/font.css'
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
